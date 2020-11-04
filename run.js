382666152ed5b2495c6bdd9d76ece6f84f7ba6e2
maxCardo <40762268+maxCardo@users.noreply.github.com>
2020-11-04 12:51:39 -0500

    reworked sheets script. works

--- run.js
+++ run.js
@@ -1,6 +1,6 @@
 const dbConnect = require('./server/db/db');
 const Company = require('./server/db/models/company');
-const {getAll, getPastQuote} = require('./server/services/fmp');
+const { getAll, getPastQuote, getBlnceSheet, getStockData, getQuote} = require('./server/services/fmp');
 const { fmpKey } = require('./server/config/creds');
 const { getTickers } = require('./server/rawData/script');
 
@@ -16,14 +16,11 @@ const activeTickers = async () => {
 const test = async () => {
 
   const activeTkrsArr = await activeTickers()
-  console.log(activeTkrsArr.length); 
-  const {data: nasdaq} = await getAll('nasdaq');
-  console.log(nasdaq.length);
-  const {data: nyse} = await getAll('nyse');
-  console.log(nyse.length);
+  const nasdaq = await getAll('nasdaq');
+  const nyse = await getAll('nyse');
   const market = [...nyse, ...nasdaq].filter(record => activeTkrsArr.includes(record.symbol) === true && record.price > 1 )
 
-  const losers = market.sort((a,b) => a.changesPercentage - b.changesPercentage).slice(0,5)
+  const losers = market.sort((a,b) => a.changesPercentage - b.changesPercentage).slice(0,15)
 
 
 
@@ -37,95 +34,110 @@ const test = async () => {
   const debtMax = .5
 
   //loop through array of records
-  losers.forEach((record) => {
-    //ToDO: if already on list dont run record ???
+  losers.forEach(async (record, i) => {
+    await setTimeout(async () => {
+      //ToDO: if already on list dont run record ???
+      //get external API data from fmp
+      try {
+       const kpi = await getStockData(record.symbol);
+       const balanceSheet = await getBlnceSheet(record.symbol);
+       const preC19 = await getPastQuote(record.symbol, '2020-2-20');
+       const quote = await getQuote(record.symbol);
 
-    //get external API data from fmp
-    const {debt,cash} = await getBalanceSheet(record.symbol)
-    const preCovid = await getPastQuote(record.symbol)
-    const kpi = await getKpi()
-    const price = await getQuote()[0].close
+       //set vars from fmp data
+       let preCovid = preC19.historical[0].close
+       let growth = kpi.reduce((total, next) => total + next.roic, 0) / kpi.length;
+       let peRatio = kpi.reduce((total, next) => total + next.peRatio, 0) / kpi.length;
+       const eps = kpi[0].netIncomePerShare;
+       const debt = balanceSheet[0].totalDebt;
+       const cash = balanceSheet[0].cashAndCashEquivalents;
+       const debtCoverage = cash / debt;
+       const price = quote[0].price;
 
-    //set vars from fmp data
-    let growth //average roic over last 3 years
-    let peRatio //get PE ratio over last 3 years
-    const eps //current eps
-    const debtCoverage // = cash/debt
+       //set dynamic target vars
+       let growthCap;
+       const growthObj = { tier1: 0.09, tier2: 0.06, tier3: 0.03, tier4: 0.0 };
+       let discount;
+       const discountObj = {
+         tier1: 0.1,
+         tier2: 0.15,
+         tier3: 0.25,
+         tier4: 0.35,
+       };
+       if (debtCoverage >= 1) {
+         growthCap = growthObj.tier1;
+         discount = discountObj.tier1;
+       } else if (debtCoverage < 1 && debtCoverage >= 0.75) {
+         growthCap = growthObj.tier2;
+         discount = discountObj.tier2;
+       } else if (debtCoverage < 0.75 && debtCoverage >= 0.5) {
+         growthCap = growthObj.tier3;
+         discount = discountObj.tier3;
+       } else {
+         growthCap = growthObj.tier4;
+         discount = discountObj.tier4;
+       }
 
-    //set dynamic target vars    
-    let growthCap
-    const growthObj = {tier1:.09,tier2:.06,tier3:.03,tier4:.0}
-    let discount
-    const discountObj = {tier1:.10,tier2:.15,tier3:.25,tier4:.35}
-    if(debtCoverage >= 1){
-      growthCap = growthObj.tier1
-      discount = discountObj.tier1
-    } else if(debtCoverage <1 && debtCoverage >=.75){
-      growthCap = growthObj.tier2
-      discount = discountObj.tier2
-    }else if(debtCoverage <.75 && debtCoverage >=.5){
-      growthCap = growthObj.tier3
-      discount = discountObj.tier3
-    }else{
-      growthCap = growthObj.tier4
-      discount = discountObj.tier4
-    }
+       //set adjusted vars based on static target vars
+       if (peRatio > capPE) {
+         peRatio = capPE;
+       } else if (peRatio < 0) {
+         peRatio = 0;
+       }
+       if (growth > growthCap) {
+         growth = growthCap;
+       }
 
-    //set adjusted vars based on static target vars
-    if(peRatio > capPE){peRatio = capPE} else if (peRatio < 0){peRatio = 0}
-    if(growth > growthCap){growth = growthCap}
-    
-    
-    //set "intrinsic" value of stock
-    const value = (((eps * growth) + eps)*peRatio)/(1+discount)
+       //set "intrinsic" value of stock
+       console.log('eps: ', eps);
+       console.log('growth: ', growth);
+       console.log('PE: ', peRatio);
+       console.log('discount: ', discount);
+       const value = ((eps * growth + eps) * peRatio) / (1 + discount);
 
-    //set buy params
-    var upside = (value - price)/price
-    var upsidePass
-    if(upside >= upsideMin){upsidePass = true}else{upsidePass = false}
-    var debtPass;
-    if(debtCoverage >= debtMax){debtPass = true}else{debtPass = false}
-    //compare value to pre crash levels
-    var crashComp = preCovid/value
-    var crashCompPass = false;
-    if(crashComp > covidCrashCap){crashCompPass = true}else{crashCompPass = false}
-    
-    //print res
-    if(upsidePass && crashCompPass && debtPass ){
-      console.log('pass');
-      console.log(record.symbol);
-      console.log('upside:', upsidePass, 'debt: ', debtPass,'crash: ', crashPass);
-    }else{
-      console.log('pass');
-      console.log(record.symbol);
-      console.log('upside:', upsidePass, 'debt: ', debtPass,'crash: ', crashPass);
-    }
+       //set buy params
+       var upside = (value - price) / price;
+       var upsidePass;
+       if (upside >= upsideMin) {
+         upsidePass = true;
+       } else {
+         upsidePass = false;
+       }
+       var debtPass;
+       if (debtCoverage >= debtMax) {
+         debtPass = true;
+       } else {
+         debtPass = false;
+       }
+       console.log('preCovid: ',preCovid);
+       console.log('value: ', value);
+       //compare value to pre crash levels
+       var crashComp = preCovid/ value;
+       console.log(crashComp);
+       var crashCompPass = false;
+       if (crashComp > covidCrashCap) {
+         crashCompPass = true;
+       } else {
+         crashCompPass = false;
+       }
 
-  })
-   
-  
-
-
-
-
-
-  //console.log('nasdaq: ', nasdaq.data.length)
-  
-  // const movers = await getTopMovers();
-  // console.log('movers: ', movers.data.length)
-  
-  // const gainers = await getTopGainers();
-  // console.log('gainers: ', gainers.data.length);
-
-  // const losers = await getTopLosers();
-  // console.log('losers: ', losers.data.length);
-
+       //print res
+       if (upsidePass && crashCompPass && debtPass) {
+         console.log(record.symbol);
+         console.log('pass');
+         console.log(' upside:', upsidePass, ' debt:', debtPass, ' crash:', crashCompPass);
+       } else {
+         console.log(record.symbol);
+         console.log('fail');
+         console.log(' upside:', upsidePass, ' debt:', debtPass, ' crash:', crashCompPass );
+       } 
+      } catch (err) {
+        console.log('error fired on ', record.symbol);
+      }
+    }, i* 1000);
+  });  
 };
 test()
-
-
-
-
 
 module.exports = {getTickers}
 
--- server/services/fmp.js
+++ server/services/fmp.js
@@ -2,56 +2,72 @@ const axios = require('axios')
 const {fmpKey} = require('../config/creds')
 
 // @desc: get data on single ticker by quarter
-const getStockData = async (tikr) => await axios({
-    url: `https://fmpcloud.io/api/v3/key-metrics/${tikr}?period=quarter&limit=130&apikey=${fmpKey}`,
-    method: 'get',
-})
+const getStockData = async (tikr) =>{
+  try {
+    const res = await axios({
+      url: `https://fmpcloud.io/api/v3/key-metrics/${tikr}?limit=3&apikey=${fmpKey}`,
+      method: 'get',
+    });
+    return res.data  
+  } catch (err) {
+    console.log('error fires getStockData');
+    //console.error(err);
+  }
+}
 
-const getBlnceSheet = async (tikr) => await axios({
-    url: `https://fmpcloud.io/api/v3/balance-sheet-statement/${tikr}?period=quarter&apikey=${fmpKey}`,
-    method: 'get',
-})
+const getBlnceSheet = async (tikr) =>{
+  try {
+   const res = await axios({
+     url: `https://fmpcloud.io/api/v3/balance-sheet-statement/${tikr}?period=quarter&limit=1&apikey=${fmpKey}`,
+     method: 'get',
+   });
+   return res.data 
+  } catch (err) {
+    console.log('error fired getBalnceSheet');
+    
+  }
+   
+}
 
-const getPastQuote = async (tikr, startDate, endDate) =>
-  await axios({
-    url: `https://fmpcloud.io/api/v3/historical-price-full/${tikr}?from=${startDate}&to=${endDate}&apikey=${fmpKey}`,
-    method: 'get',
-  });
+const getPastQuote = async (tikr, startDate, endDate) =>{
+  try {
+    const res = await axios({
+      url: `https://fmpcloud.io/api/v3/historical-price-full/${tikr}?from=${startDate}&to=${endDate? endDate: startDate}&apikey=${fmpKey}`,
+      method: 'get',
+    });
+    return res.data
 
-const getQuote = async (tikr) =>await axios({
-    url: `https://fmpcloud.io/api/v3/quote/${tikr}?apikey=${fmpKey}`,
-    method: 'get',
-})
+  } catch (err) {
+    console.log('error fired getPastQuote');
+        
+  }
+}
 
-// @desc: returns 30 of the top movers
-const getTopMovers = async () =>await axios({
-  url: `https://fmpcloud.io/api/v3/actives?apikey=${fmpKey}`,
-  method: 'get',
-});
-
-// @desc: returns 30 of top gainers
-const getTopGainers = async () =>await axios({
-  url: `https://fmpcloud.io/api/v3/actives?apikey=${fmpKey}`,
-  method: 'get',
-});
-
-// @desc: returns 30 of top losers
-const getTopLosers = async () =>await axios({
-  url: `https://fmpcloud.io/api/v3/actives?apikey=${fmpKey}`,
-  method: 'get',
-});
+const getQuote = async (tikr) =>{
+  try {
+   const res = await axios({
+     url: `https://fmpcloud.io/api/v3/quote/${tikr}?apikey=${fmpKey}`,
+     method: 'get',
+   });
+   return res.data 
+  } catch (err) {
+    console.log('errror fired getQuote');
+        
+  }
+}
 
 // @desc: get all tickers in exchage. Available: nyse, nasdaq, etf
-const getAll = async (exchange) =>await axios({
-  url: `https://fmpcloud.io/api/v3/quotes/${exchange}?apikey=${fmpKey}`,
-  method: 'get',
-});
+const getAll = async (exchange) =>{
+  try {
+    const res = await axios({
+      url: `https://fmpcloud.io/api/v3/quotes/${exchange}?apikey=${fmpKey}`,
+      method: 'get',
+    });
+    return res.data  
+  } catch (err) {
+    console.log('error fired on getAll');
+    
+  }
+}
 
-// @desc: 
-const getDelisted = async (exchange) =>await axios({
-  url: `https://fmpcloud.io/api/v3/delisted-companies?apikey=${fmpKey}`,
-  method: 'get',
-});
-
-
-module.exports = {getStockData, getBlnceSheet, getPastQuote, getQuote, getTopMovers, getTopGainers, getTopLosers, getAll, getDelisted}
+module.exports = {getStockData, getBlnceSheet, getPastQuote, getQuote, getAll}
