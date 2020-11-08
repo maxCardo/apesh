52f2f3197151cbcdbbfebe604adf61b9914a42b2
maxCardo <40762268+maxCardo@users.noreply.github.com>
2020-11-08 18:47:50 -0500

    deleted unused schema

--- server/db/models/watchlist/watchlist.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const watchlistSchema = new mongoose.Schema({
-  active,
-  user,
-})
-
-module.exports = Chat = mongoose.model('Watchlist', watchlistSchema);
