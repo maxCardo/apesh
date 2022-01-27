55554da9d72427522349dd229a36000725d751e4
maxCardo <adampoznanski@outlook.com>
2022-01-27 10:11:11 -0500

    fixed sorting

--- client/src/components/custom/scaner/scanner2.js
+++ client/src/components/custom/scaner/scanner2.js
@@ -1,29 +1,22 @@
 import React, {useEffect} from 'react';
 import {connect} from 'react-redux'
 
-import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import { lighten, makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
 import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
-import TableSortLabel from '@material-ui/core/TableSortLabel';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import Checkbox from '@material-ui/core/Checkbox';
-import IconButton from '@material-ui/core/IconButton';
-import Tooltip from '@material-ui/core/Tooltip';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Switch from '@material-ui/core/Switch';
-import DeleteIcon from '@material-ui/icons/Delete';
-import FilterListIcon from '@material-ui/icons/FilterList';
 
 import {getScanner} from '../../../actions/scanner'
+import {useStyles, useToolbarStyles} from './styles'
+import {stableSort, getComparator, getData} from './scripts'
+import TableHeader from './TableHeader';
+import EnhancedToolbar from './EnhancedToolbar';
 
 const headers = [
   {
@@ -54,200 +47,6 @@ const headers = [
   },
 ]
 
-const getData = function (dataItem, header) {
-  if (!dataItem || !header) return null
-  if (header.reactComponent) {
-      return header.render(dataItem)
-  } else {
-      const { accessor } = header
-      if (accessor.includes('.')) {
-          const accessorsArray = accessor.split('.')
-          let item = dataItem;
-          accessorsArray.forEach((accessor) => {
-              if (item) {
-                  item = item[accessor]
-              }
-          })
-          return item
-      } else {
-          return dataItem[accessor]
-      }
-  }
-};
-  
-
-function descendingComparator(a, b, orderBy) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
-
-function getComparator(order, orderBy) {
-  return order === 'desc'
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-function stableSort(array, comparator) {
-  const stabilizedThis = array.map((el, index) => [el, index]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) return order;
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map((el) => el[0]);
-}
-
-const headCells = [
-  { id: 'name', numeric: false, disablePadding: true, label: 'Dessert (100g serving)' },
-  { id: 'calories', numeric: true, disablePadding: false, label: 'Calories' },
-  { id: 'fat', numeric: true, disablePadding: false, label: 'Fat (g)' },
-  { id: 'carbs', numeric: true, disablePadding: false, label: 'Carbs (g)' },
-  { id: 'protein', numeric: true, disablePadding: false, label: 'Protein (g)' },
-];
-
-function EnhancedTableHead(props) {
-  const { classes, onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } = props;
-  const createSortHandler = (property) => (event) => {
-    onRequestSort(event, property);
-  };
-
-  return (
-    <TableHead>
-      <TableRow>
-        <TableCell padding="checkbox">
-          <Checkbox
-            indeterminate={numSelected > 0 && numSelected < rowCount}
-            checked={rowCount > 0 && numSelected === rowCount}
-            onChange={onSelectAllClick}
-            inputProps={{ 'aria-label': 'select all desserts' }}
-          />
-        </TableCell>
-        {headers.map((rec) => (
-          <TableCell
-            key={rec.label}
-            align={rec.allignRight ? 'right' : 'left'}
-            padding={rec.disablePadding ? 'none' : 'normal'}
-            sortDirection={orderBy === rec.accessor ? order : false}
-          >
-            <TableSortLabel
-              active={orderBy === rec.accessor}
-              direction={orderBy === rec.accessor ? order : 'asc'}
-              onClick={createSortHandler(rec.label)}
-            >
-              {rec.label}
-              {orderBy === rec.accessor ? (
-                <span className={classes.visuallyHidden}>
-                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                </span>
-              ) : null}
-            </TableSortLabel>
-          </TableCell>
-        ))}
-      </TableRow>
-    </TableHead>
-  );
-}
-
-EnhancedTableHead.propTypes = {
-  classes: PropTypes.object.isRequired,
-  numSelected: PropTypes.number.isRequired,
-  onRequestSort: PropTypes.func.isRequired,
-  onSelectAllClick: PropTypes.func.isRequired,
-  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
-  orderBy: PropTypes.string.isRequired,
-  rowCount: PropTypes.number.isRequired,
-};
-
-const useToolbarStyles = makeStyles((theme) => ({
-  root: {
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(1),
-  },
-  highlight:
-    theme.palette.type === 'light'
-      ? {
-          color: theme.palette.secondary.main,
-          backgroundColor: lighten(theme.palette.secondary.light, 0.85),
-        }
-      : {
-          color: theme.palette.text.primary,
-          backgroundColor: theme.palette.secondary.dark,
-        },
-  title: {
-    flex: '1 1 100%',
-  },
-}));
-
-const EnhancedTableToolbar = (props) => {
-  const classes = useToolbarStyles();
-  const { numSelected } = props;
-
-  return (
-    <Toolbar
-      className={clsx(classes.root, {
-        [classes.highlight]: numSelected > 0,
-      })}
-    >
-      {numSelected > 0 ? (
-        <Typography className={classes.title} color="inherit" variant="subtitle1" component="div">
-          {numSelected} selected
-        </Typography>
-      ) : (
-        <Typography className={classes.title} variant="h6" id="tableTitle" component="div">
-          Nutrition
-        </Typography>
-      )}
-
-      {numSelected > 0 ? (
-        <Tooltip title="Delete">
-          <IconButton aria-label="delete">
-            <DeleteIcon />
-          </IconButton>
-        </Tooltip>
-      ) : (
-        <Tooltip title="Filter list">
-          <IconButton aria-label="filter list">
-            <FilterListIcon />
-          </IconButton>
-        </Tooltip>
-      )}
-    </Toolbar>
-  );
-};
-
-EnhancedTableToolbar.propTypes = {
-  numSelected: PropTypes.number.isRequired,
-};
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%',
-  },
-  paper: {
-    width: '100%',
-    marginBottom: theme.spacing(2),
-  },
-  table: {
-    minWidth: 750,
-  },
-  visuallyHidden: {
-    border: 0,
-    clip: 'rect(0 0 0 0)',
-    height: 1,
-    margin: -1,
-    overflow: 'hidden',
-    padding: 0,
-    position: 'absolute',
-    top: 20,
-    width: 1,
-  },
-}));
-
 const  Scanner2 = ({scanner: {loading, list}, getScanner}) => {
   const classes = useStyles();
   const [order, setOrder] = React.useState('asc');
@@ -255,22 +54,17 @@ const  Scanner2 = ({scanner: {loading, list}, getScanner}) => {
   const [selected, setSelected] = React.useState([]);
   const [page, setPage] = React.useState(0);
   const [dense, setDense] = React.useState(false);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
   useEffect(() => {
     getScanner()
     setOrderBy(headers[0].accessor)
   },[])
 
-  const handleRequestSort = (event, property) => {
-    const isAsc = orderBy === property && order === 'asc';
-    setOrder(isAsc ? 'desc' : 'asc');
-    setOrderBy(property);
-  };
-
+ 
   const handleSelectAllClick = (event) => {
     if (event.target.checked) {
-      const newSelecteds = headers.map((n) => n.accessor);
+      const newSelecteds = list.map((n) => n.symbol);
       setSelected(newSelecteds);
       return;
     }
@@ -293,8 +87,16 @@ const  Scanner2 = ({scanner: {loading, list}, getScanner}) => {
         selected.slice(selectedIndex + 1),
       );
     }
-
     setSelected(newSelected);
+  };
+
+  const handleRequestSort = (event, property) => {
+    console.log(property, orderBy, order)
+    const isAsc = orderBy === property && order === 'asc';
+    console.log(isAsc)
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+    console.log(property, orderBy, order)
   };
 
   const handleChangePage = (event, newPage) => {
@@ -302,7 +104,7 @@ const  Scanner2 = ({scanner: {loading, list}, getScanner}) => {
   };
 
   const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
+    setRowsPerPage(parseInt(event.target.value));
     setPage(0);
   };
 
@@ -312,20 +114,35 @@ const  Scanner2 = ({scanner: {loading, list}, getScanner}) => {
 
   const isSelected = (name) => selected.indexOf(name) !== -1;
 
-  const emptyRows = rowsPerPage - Math.min(rowsPerPage, list.length - page * rowsPerPage);
+  //const emptyRows = rowsPerPage - Math.min(rowsPerPage, list.length - page * rowsPerPage);
+  const emptyRows = 0;
 
   return (
     <div className={classes.root}>
       <Paper className={classes.paper}>
-        <EnhancedTableToolbar numSelected={selected.length} />
+        
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25,50, 100]}
+          component="div"
+          count={list.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+
+        <EnhancedToolbar numSelected={selected.length} classes={useToolbarStyles()} />
+        
         <TableContainer>
+       
           <Table
             className={classes.table}
             aria-labelledby="tableTitle"
             size={dense ? 'small' : 'medium'}
             aria-label="enhanced table"
           >
-            <EnhancedTableHead
+            <TableHeader
+              headers = {headers}
               classes={classes}
               numSelected={selected.length}
               order={order}
@@ -381,15 +198,7 @@ const  Scanner2 = ({scanner: {loading, list}, getScanner}) => {
             </TableBody>
           </Table>
         </TableContainer>
-        <TablePagination
-          rowsPerPageOptions={[5, 10, 25]}
-          component="div"
-          count={list.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
+       
       </Paper>
       <FormControlLabel
         control={<Switch checked={dense} onChange={handleChangeDense} />}
